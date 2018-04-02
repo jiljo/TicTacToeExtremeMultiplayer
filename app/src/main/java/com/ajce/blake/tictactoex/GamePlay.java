368b59d3ac455ca4d0e818cb3abf7c3632706565
jismonkj <jismonkj@mca.ajce.in>
2018-04-02 09:36:17 +0530

    3x3 table layout design game play functions

--- app/src/main/java/com/ajce/blake/tictactoex/GamePlay.java
+++ app/src/main/java/com/ajce/blake/tictactoex/GamePlay.java
@@ -2,13 +2,127 @@ package com.ajce.blake.tictactoex;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class GamePlay extends AppCompatActivity {
+public class GamePlay extends AppCompatActivity implements View.OnClickListener {
+
+    //
+    private Button[][] buttons = new Button[3][3];
+    private boolean player1Turn = true;
+    private int roundCount;
+    private int player1Points;
+    private int player2Points;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_play);
+        setContentView(R.layout.game_play);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                String buttonID = "button_" + i + j;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[i][j] = findViewById(resID);
+                buttons[i][j].setOnClickListener(this);
+            }
+        }
 
     }
+
+    public void buClick(View view) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (!((Button) v).getText().toString().equals("")) {
+            return;
+        }
+
+        if (player1Turn) {
+            ((Button) v).setText("X");
+        } else {
+            ((Button) v).setText("O");
+        }
+
+        roundCount++;
+
+        if (checkForWin()) {
+            if (player1Turn) {
+                player1Wins();
+            } else {
+                player2Wins();
+            }
+        } else if (roundCount == 9) {
+            draw();
+        } else {
+            player1Turn = !player1Turn;
+        }
+    }
+
+
+    //utility methods
+    private boolean checkForWin() {
+        String[][] field = new String[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = buttons[i][j].getText().toString();
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (field[i][0].equals(field[i][1])
+                    && field[i][0].equals(field[i][2])
+                    && !field[i][0].equals("")) {
+                return true;
+            }
+        }//row
+
+        for (int i = 0; i < 3; i++) {
+            if (field[0][i].equals(field[1][i])
+                    && field[0][i].equals(field[2][i])
+                    && !field[0][i].equals("")) {
+                return true;
+            }
+        }//column
+
+        if (field[0][0].equals(field[1][1])
+                && field[0][0].equals(field[2][2])
+                && !field[0][0].equals("")) {
+            return true;
+        }//diagonally left to right
+
+        return field[0][2].equals(field[1][1])
+                && field[0][2].equals(field[2][0])
+                && !field[0][2].equals("");
+
+    }
+
+    private void player1Wins() {
+        Toast.makeText(this, "Player 1 Wins", Toast.LENGTH_SHORT).show();
+        resetPlay();
+    }
+
+    private void player2Wins() {
+        Toast.makeText(this, "Player 2 Wins", Toast.LENGTH_SHORT).show();
+        resetPlay();
+    }
+
+    private void draw() {
+        Toast.makeText(this, "It's a Draw!", Toast.LENGTH_SHORT).show();
+        resetPlay();
+    }
+
+    private void resetPlay() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+            }
+        }
+    }
+
+
 }
